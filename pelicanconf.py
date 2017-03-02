4a680284bc773d4ce3aeac5ac9e66e1442470785
Riley Avron <ra.git@posteo.net>
2017-03-02 11:35:15 -0800

    Implement favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,10 +23,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # TODO: Edit css to roundify this
 SITEIMAGE = 'https://www.gravatar.com/avatar/b6dda2f05f2182e4ad1cf20cb6fa9872?s=128'
@@ -41,6 +42,20 @@ ICONS = (
 )
 
 DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['extras', 'images']
+EXTRA_PATH_METADATA = {
+        'extras/android-chrome-192x192.png': {'path': 'android-chrome-192x192.png'},
+        'extras/android-chrome-512x512.png': {'path': 'android-chrome-512x512.png'},
+        'extras/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+        'extras/browserconfig.xml': {'path': 'browserconfig.xml'},
+        'extras/favicon-16x16.png': {'path': 'favicon-16x16.png'},
+        'extras/favicon-32x32.png': {'path': 'favicon-32x32.png'},
+        'extras/favicon.ico': {'path': 'favicon.ico'},
+        'extras/manifest.json': {'path': 'manifest.json'},
+        'extras/mstile-150x150.png': {'path': 'mstile-150x150.png'},
+        'extras/safari-pinned-tab.svg': {'path': 'safari-pinned-tab.svg'},
+}
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
