e4fa786a0676708c2fc9a3368fd226a9acdbc871
Riley Avron <ravron@dropbox.com>
2017-03-02 11:38:03 -0800

    Remove feeds from publish conf

--- publishconf.py
+++ publishconf.py
@@ -13,11 +13,6 @@ from pelicanconf import *
 SITEURL = 'http://www.ravron.com'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-
-DELETE_OUTPUT_DIRECTORY = True
-
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
